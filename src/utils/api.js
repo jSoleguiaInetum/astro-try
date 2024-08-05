d948aae2fb627078c91a585e4f94d14864fbdcab
J Soleguia <133122850+jSoleguiaInetum@users.noreply.github.com>
2024-08-05 13:37:16 +0200

    Controla si las llamadas devuelven información o no...

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,25 +4,32 @@ const getBooksList = async () => {
 	const apiAction = 'search.json';
 	let apiParams = ['q=storm+front', 'limit=10', 'fields=key,title,author_name'];
 	const apiCallUrl = `${apiBaseUrl}${apiAction}?${apiParams.join('&')}`;
-	const booksApi = await fetch(apiCallUrl).then((res) => res.json());
-	return booksApi.docs.map((book) => {
-		return {
-			...book,
-			key: book.key.replace('/works/', ''),
-		};
-	});
+	let output = null;
+	try {
+		const booksApi = await fetch(apiCallUrl).then((res) => res.json());
+		output = booksApi.docs.map((book) => {
+			return {
+				...book,
+				key: book.key.replace('/works/', ''),
+			};
+		});
+	} catch (error) {
+		console.log(error);
+	}
+	return output;
 };
 
 const getBookByKey = async (key) => {
 	const apiAction = 'works';
 	const apiCallUrl = `${apiBaseUrl}${apiAction}/${key}.json`;
+	let output = null;
+
 	try {
-		const bookData = await fetch(apiCallUrl).then((res) => res.json());
-		return bookData;
+		output = await fetch(apiCallUrl).then((res) => res.json());
 	} catch (error) {
 		console.log(error);
-		return null;
 	}
+	return output;
 };
 
 const apiCoversUrl = 'https://covers.openlibrary.org/b/';
@@ -31,16 +38,15 @@ const apiCoversUrl = 'https://covers.openlibrary.org/b/';
 const getBookCoverByCoverId = async (coverId, key = 'id', size = 'M') => {
 	const coverUrl = `${key.toLowerCase()}/${coverId}-${size}.jpg`;
 	const apiCallUrl = `${apiCoversUrl}${coverUrl}`;
-	console.log({ apiCallUrl });
+	let output = null;
 	try {
-		const cover = await fetch(apiCallUrl).then((res) => {
+		output = await fetch(apiCallUrl).then((res) => {
 			return res.url;
 		});
-		return cover;
 	} catch (error) {
 		console.log(error);
-		return null;
 	}
+	return output;
 };
 
 export { getBooksList, getBookByKey, getBookCoverByCoverId };
