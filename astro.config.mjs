38bb40040a50fd69d1cf70a16025539b861101b2
J Soleguia <133122850+jSoleguiaInetum@users.noreply.github.com>
2024-08-08 08:51:04 +0200

    Añade auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import vue from '@astrojs/vue';
 
+import auth from 'auth-astro';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [vue({ jsx: true })],
+	integrations: [
+		vue({
+			jsx: true,
+		}),
+		auth(),
+	],
 	output: 'server',
 	adapter: netlify(),
 });
-
