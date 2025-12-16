270c3fc226ec4b8eaab77a1d59a4bc30e0218208
J Soleguia <133122850+jSoleguiaInetum@users.noreply.github.com>
2025-12-16 15:33:42 +0100

    Añade linkedin oAuth

--- auth.config.mjs
+++ auth.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'auth-astro';
 import Google from '@auth/core/providers/google';
+import LinkedIn from "@auth/core/providers/linkedin"
 
 export default defineConfig({
 	providers: [
 		Google({
 			clientId: import.meta.env.GOOGLE_CLIENT_ID,
 			clientSecret: import.meta.env.GOOGLE_CLIENT_SECRET,
+		}),
+		LinkedIn({
+			clientId: import.meta.env.AUTH_LINKEDIN_ID,
+			clientSecret: import.meta.env.AUTH_LINKEDIN_SECRET,
 		}),
 	],
 });
